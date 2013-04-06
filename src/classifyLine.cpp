78c4ef3fdc336f5a26d8be526331d5891e0fecdc
Calvin Tam <calvinyhtam@gmail.com>
2013-04-06 18:52:51 +1100

    Fills detected T intersection

--- src/classifyLine.cpp
+++ src/classifyLine.cpp
@@ -1,10 +1,13 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-
 #include <iostream>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
+
+bool inRange(double x, double min, double max);
+double radToDeg(double thetaInRad);
 
 void help()
 {
@@ -31,39 +34,88 @@ int main(int argc, char** argv)
 
     vector<Vec4i> lines;
     HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 80 );
-    Point bestTopLeft(320,240);
-    Point bestBotLeft(320,240);
-    Point bestTopRight(320,240);
-    Point bestBotRight(320,240);
+    Point hBestTopLeft(320,240);
+    Point hBestBotLeft(320,240);
+    Point hBestTopRight(320,240);
+    Point hBestBotRight(320,240);
+    Point vBestTopLeft(415,240);
+    Point vBestBotLeft(415,240);
+    Point vBestTopRight(415,240);
+    Point vBestBotRight(415,240);
     for( size_t i = 0; i < lines.size(); i++ )
     {
         Vec4i l = lines[i];
+        // Top left corner of image is (0,0)
         // End points of detected lines
         Point pts[] = {Point(l[0], l[1]),
                        Point(l[2], l[3])};
-        // Top left corner of image is (0,0)
-        for(int i = 0; i < 2; ++i) {
-            if(pts[i].x < bestTopLeft.x && pts[i].y > 80 && pts[i].y < 115) {
-                bestTopLeft = pts[i];
+
+        cout << "x0 = " << l[0] << endl;
+        cout << "y0 = " << l[1] << endl;
+        cout << "x1 = " << l[2] << endl;
+        cout << "y1 = " << l[3] << endl;
+        // then theta is the principal arctan of the line (atan not atan2)
+        // Hence -90 < radToDeg(theta) < 90
+        double thetaInRad = (l[2] == l[0]) ? M_PI/2 : atan((double)(l[3]-l[1])/(l[2]-l[0]));
+        double thetaInDeg = radToDeg(thetaInRad);
+        cout << "thetaInDeg = " << thetaInDeg << endl;
+        // For the almost horizontal field line
+        // Only consider detected line segments with -20 < theta < 20 
+        if(inRange(thetaInDeg,-20,20)) {
+            for(int i = 0; i < 2; ++i) {
+                if(pts[i].x < hBestTopLeft.x && pts[i].y > 80 && pts[i].y < 115) {
+                    hBestTopLeft = pts[i];
+                }
+                if(pts[i].x < hBestBotLeft.x && pts[i].y > 115) {
+                    hBestBotLeft = pts[i];
+                }
+                if(pts[i].x > hBestTopRight.x && pts[i].y > 120 && pts[i].y < 140) {
+                    hBestTopRight = pts[i];
+                }
+                if(pts[i].x > hBestBotRight.x && pts[i].y > 140) {
+                    hBestBotRight = pts[i];
+                }
             }
-            if(pts[i].x < bestBotLeft.x && pts[i].y > 115) {
-                bestBotLeft = pts[i];
-            }
-            if(pts[i].x > bestTopRight.x && pts[i].y > 120 && pts[i].y < 140) {
-                bestTopRight = pts[i];
-            }
-            if(pts[i].x > bestBotRight.x && pts[i].y > 140) {
-                bestBotRight = pts[i];
+        }
+
+        // For the almost vertical field line
+        // Only consider detected line segments with  70 < theta < 90 && -90 < theta < -70
+        if(inRange(thetaInDeg,70,90) || inRange(thetaInDeg,-90,-70)) {
+            for(int i = 0; i < 2; ++i) {
+                if(pts[i].x < vBestTopLeft.x && pts[i].y > 100 && pts[i].y < 240) {
+                    vBestTopLeft = pts[i];
+                }
+                if(pts[i].x < vBestBotLeft.x && pts[i].y > 240) {
+                    vBestBotLeft = pts[i];
+                }
+                if(pts[i].x > vBestTopRight.x && pts[i].y > 100 && pts[i].y < 240) {
+                    vBestTopRight = pts[i];
+                }
+                if(pts[i].x > vBestBotRight.x && pts[i].y > 240) {
+                    vBestBotRight = pts[i];
+                }
             }
         }
         //line( cdst, pt_a, pt_b, Scalar(0,0,255), 1, CV_AA);
     }
 
-    Point pts[] = {bestTopLeft,bestTopRight,bestBotRight,bestBotLeft};
-    fillConvexPoly(cdst,&pts[0],4,Scalar(255,255,255), CV_AA, 0);
+    cout << "====== Vertical line corners =====" << endl;
+    cout << "vBestTopLeft=" <<  vBestTopLeft<< endl;
+    cout << "vBestTopRight=" <<  vBestTopRight<< endl;
+    cout << "vBestBotRight=" <<  vBestBotRight<< endl;
+    cout << "vBestBotLeft=" <<  vBestBotLeft<< endl;
 
-    line(cdst,bestTopLeft,bestTopRight,Scalar(0,255,0),1,CV_AA);
-    line(cdst,bestBotLeft,bestBotRight,Scalar(255,0,0),1,CV_AA);
+    Point h_pts[] = {hBestTopLeft,hBestTopRight,hBestBotRight,hBestBotLeft};
+    Point v_pts[] = {vBestTopLeft,vBestTopRight,vBestBotRight,vBestBotLeft};
+
+    fillConvexPoly(cdst,&h_pts[0],4,Scalar(255,255,255), CV_AA, 0);
+    fillConvexPoly(cdst,&v_pts[0],4,Scalar(255,255,255), CV_AA, 0);
+    
+    line(cdst,hBestTopLeft,hBestTopRight,Scalar(0,255,0),1,CV_AA);
+    line(cdst,hBestBotLeft,hBestBotRight,Scalar(0,255,0),1,CV_AA);
+
+    line(cdst,vBestTopLeft,vBestBotLeft,Scalar(0,0,255),1,CV_AA);
+    line(cdst,vBestTopRight,vBestBotRight,Scalar(0,0,255),1,CV_AA);
     
     namedWindow("source", CV_WINDOW_NORMAL);
     imshow("source", src);
@@ -73,4 +125,13 @@ int main(int argc, char** argv)
     waitKey();
 
     return 0;
+}
+
+
+double radToDeg(double thetaInRad) {
+    return thetaInRad * 180 / M_PI;
+}
+
+bool inRange(double x, double min, double max) {
+    return (x > min && x < max ? true : false);
 }
