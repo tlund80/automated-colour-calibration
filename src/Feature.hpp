aa65c4e895bec72575ca31760d216a09e89b38ed
Calvin Tam <calvinyhtam@gmail.com>
2013-04-10 20:26:24 +1000

    Added qualifiers for header

--- src/Feature.hpp
+++ src/Feature.hpp
@@ -1,17 +1,16 @@
-using namespace std;
 using namespace cv;
 
 class Feature {
 public: 
-    vector<Point> region;
-    vector<Rect> vertexBounds;
+    std::vector<cv::Point> region;
+    std::vector<cv::Rect> vertexBounds;
     int minAbsAngle;
     int maxAbsAngle;
 
-    Feature(vector<Point> &region, vector<Rect> &vertexBounds, int minAbsAngle, int maxAbsAngle);
+    Feature(std::vector<cv::Point> &region, std::vector<cv::Rect> &vertexBounds, int minAbsAngle, int maxAbsAngle);
 };
 
-Feature::Feature(vector<Point> &region, vector<Rect> &vertexBounds, int minAbsAngle, int maxAbsAngle) {
+Feature::Feature(std::vector<cv::Point> &region, std::vector<cv::Rect> &vertexBounds, int minAbsAngle, int maxAbsAngle) {
     this->region = region;
     this->vertexBounds = vertexBounds;
     this->minAbsAngle = minAbsAngle;
