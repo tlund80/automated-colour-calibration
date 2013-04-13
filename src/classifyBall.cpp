17fb8e9cce330ecef711026ca9d28a07b95fafb5
Calvin Tam <calvinyhtam@gmail.com>
2013-04-13 12:44:11 +1000

    Remove useless displays and renamed them

--- src/classifyBall.cpp
+++ src/classifyBall.cpp
@@ -17,11 +17,10 @@ int main(int argc, char** argv)
     if( !srcOrig.data )
     { return -1; }
 
-    namedWindow( "gray", CV_WINDOW_NORMAL);
-    namedWindow( "Hough Circle", CV_WINDOW_NORMAL);
-    namedWindow("high_contrast", CV_WINDOW_NORMAL);
-    namedWindow("trackbar", CV_WINDOW_NORMAL);
-    namedWindow("src",CV_WINDOW_NORMAL);
+    namedWindow( "src with detected Hough Circles", CV_WINDOW_NORMAL);
+    namedWindow("edgeMap",CV_WINDOW_NORMAL);
+    namedWindow("saturated", CV_WINDOW_NORMAL);
+
     double alpha=1.0; // 1.0 - 3.0
     double beta=0; // 0-100
 
@@ -40,53 +39,47 @@ int main(int argc, char** argv)
         Mat src = srcOrig.clone();
         Mat src_gray = src.clone();
         cvtColor(src_gray,src_gray,CV_BGR2GRAY);
-        //  cvtColor(src_gray_1,src_gray,CV_GRAY2BGR);
+        //  cvtColor(edgeMap,src_gray,CV_GRAY2BGR);
 
-        Mat new_image = Mat::zeros( src.size(), src.type() );
-        /// Do the operation new_image(i,j) = alpha*image(i,j) + beta
+        Mat saturated = Mat::zeros( src.size(), src.type() );
+        /// Do the operation saturated(i,j) = alpha*image(i,j) + beta
         for( int y = 0; y < src.rows; y++ )
         { for( int x = 0; x < src.cols; x++ )
             { for( int c = 0; c < 3; c++ )
                 {
-                    new_image.at<Vec3b>(y,x)[c] =
+                    saturated.at<Vec3b>(y,x)[c] =
                         saturate_cast<uchar>( alpha*( src.at<Vec3b>(y,x)[c] ) + beta );
                 }
             }
 
         }
-        Mat temp;
-        cvtColor(new_image,temp,CV_BGR2GRAY);
-        Mat src_gray_1;
+        Mat edgeMap;
         //cvtColor(src,src,CV_BGR2GRAY);
-        Canny(new_image,src_gray_1, 150,200,3);
-         
+        Canny(saturated,edgeMap, 150,200,3);
+
         /// Convert it to gray
-        // cvtColor(new_image, src_gray, CV_BGR2GRAY );
         /// Reduce the noise so we avoid false circle detection
-        GaussianBlur( src_gray_1, src_gray_1, Size(7, 7), 2, 2 );
-        
+        GaussianBlur( edgeMap, edgeMap, Size(7, 7), 2, 2 );
+
         vector<Vec3f> circles;
 
         /// Apply the Hough Transform to find the circles
-        HoughCircles( src_gray_1, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 100, 0, 0 );
+        HoughCircles( edgeMap, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 100, 0, 0 );
         cout << circles.size() << endl;
         /// Draw the circles detected
         for( size_t i = 0; i < circles.size(); i++ )
         {
-        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-        int radius = cvRound(circles[i][2]);
-        // circle center
-        circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
-        // circle outline
-        circle( src, center, radius, Scalar(255,0,0), 3, 8, 0 );
+            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            // circle center
+            circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+            // circle outline
+            circle( src, center, radius, Scalar(255,0,0), 3, 8, 0 );
         }
-       
-        imshow("src",src);
-        /// Show your results
-        imshow( "src_gray_1", src_gray_1);
-        
-        imshow( "Hough Circle", temp); 
-        imshow("high_contrast", new_image);
+
+        imshow("src with detected Hough Circles",src);
+        imshow( "edgeMap", edgeMap);
+        imshow("saturated", saturated);
     } while(waitKey(0) != 27);  
     return 0;
 }
