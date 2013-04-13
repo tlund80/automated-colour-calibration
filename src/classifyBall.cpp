535c9a2b664074bb88a152e77c8ebbbf2356c028
Calvin Tam <calvinyhtam@gmail.com>
2013-04-13 14:45:20 +1000

    Added bwImage and trackbars

--- src/classifyBall.cpp
+++ src/classifyBall.cpp
@@ -17,7 +17,9 @@ int main(int argc, char** argv)
     if( !srcOrig.data )
     { return -1; }
 
-    namedWindow( "src with detected Hough Circles", CV_WINDOW_NORMAL);
+    namedWindow("trackbar",CV_WINDOW_NORMAL);
+    namedWindow("bwImage",CV_WINDOW_NORMAL);
+    namedWindow("src with detected Hough Circles", CV_WINDOW_NORMAL);
     namedWindow("edgeMap",CV_WINDOW_NORMAL);
     namedWindow("saturated", CV_WINDOW_NORMAL);
 
@@ -29,6 +31,12 @@ int main(int argc, char** argv)
     createTrackbar("alpha","trackbar", &alphaLevel, 255);
     createTrackbar("beta","trackbar", &betaLevel, 255);
 
+    int thresholdType = 0;
+    int thresholdValue = 0;
+    int max_binary_value = 255;
+    string trackbar_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
+    createTrackbar(trackbar_type,"trackbar",&thresholdType,5);
+    createTrackbar("Threshold Value","trackbar",&thresholdValue,255);
     /// Initialize values
     //  std::cout<<"* Enter the alpha value [1.0-3.0]: ";std::cin>>alpha;
     //  std::cout<<"* Enter the beta value [0-100]: "; std::cin>>beta;
@@ -40,27 +48,30 @@ int main(int argc, char** argv)
 
         Mat saturated = Mat::zeros( src.size(), src.type() );
         /// Do the operation saturated(i,j) = alpha*image(i,j) + beta
-        for( int y = 0; y < src.rows; y++ )
-        { for( int x = 0; x < src.cols; x++ )
-            { for( int c = 0; c < 3; c++ )
-                {
+        for( int y = 0; y < src.rows; y++ ) { 
+            for( int x = 0; x < src.cols; x++ ) { 
+                for( int c = 0; c < 3; c++ ) {
                     saturated.at<Vec3b>(y,x)[c] =
                         saturate_cast<uchar>( alpha*( src.at<Vec3b>(y,x)[c] ) + beta );
                 }
             }
-
         }
-        Mat edgeMap;
-        Canny(saturated,edgeMap, 150,200,3);
 
-        /// Convert it to gray
+        Mat src_gray,bwImage;
+        cvtColor(src,src_gray,CV_RGB2GRAY);
+        
+        threshold(src_gray, bwImage, thresholdValue, max_binary_value,thresholdType);
+        Mat edgeMap;
+        //Canny(saturated,edgeMap, 150,200,3);
+        Canny(bwImage,edgeMap, 150,200,3);
+
         /// Reduce the noise so we avoid false circle detection
-        GaussianBlur( edgeMap, edgeMap, Size(7, 7), 2, 2 );
+        //GaussianBlur( edgeMap, edgeMap, Size(7, 7), 2, 2 );
 
         vector<Vec3f> circles;
 
         /// Apply the Hough Transform to find the circles
-        HoughCircles( edgeMap, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 100, 0, 0 );
+        HoughCircles(edgeMap, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 100, 0, 0 );
         cout << circles.size() << endl;
         /// Draw the circles detected
         for( size_t i = 0; i < circles.size(); i++ )
@@ -74,7 +85,8 @@ int main(int argc, char** argv)
         }
 
         imshow("src with detected Hough Circles",src);
-        imshow( "edgeMap", edgeMap);
+        imshow("bwImage",bwImage);
+        imshow("edgeMap", edgeMap);
         imshow("saturated", saturated);
     } while(waitKey(0) != 27);  
     return 0;
