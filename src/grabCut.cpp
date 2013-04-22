697a22a2da899ab0034edb44a661dad0dd31a290
Calvin Tam <calvinyhtam@gmail.com>
2013-04-22 11:41:32 +1000

    Fixed rgb2yuv ordering of uv pair

--- src/grabCut.cpp
+++ src/grabCut.cpp
@@ -13,8 +13,8 @@ PixelValues rgb2yuv(cv::Vec3b i) {
     int g = 1;
     int r = 2;
     double y = 0.299*i[r] + 0.587*i[g] + 0.114*i[b];
-    double u = (i[r] - y) * 0.713 + 128;
-    double v = (i[b] - y) * 0.564 + 128;
+    double u = (i[b] - y) * 0.564 + 128;
+    double v = (i[r] - y) * 0.713 + 128;
     PixelValues p;
     p.y = static_cast<uint8_t>(y); 
     p.u = static_cast<uint8_t>(u);
