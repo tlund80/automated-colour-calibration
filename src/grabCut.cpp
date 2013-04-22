bfada39fac91bafa25216e9fec3bde269ce8e309
Calvin Tam <calvinyhtam@gmail.com>
2013-04-22 11:21:21 +1000

    Added rgb2yuv conversion

--- src/grabCut.cpp
+++ src/grabCut.cpp
@@ -7,12 +7,18 @@
 using namespace cv;
 using namespace std;
 
-// no transformation for the meantime
-PixelValues rgb2yuv(cv::Vec3b rgbPixel) {
+// Note the input pixel vector is in BGR order
+PixelValues rgb2yuv(cv::Vec3b i) {
+    int b = 0;
+    int g = 1;
+    int r = 2;
+    double y = 0.299*i[r] + 0.587*i[g] + 0.114*i[b];
+    double u = (i[r] - y) * 0.713 + 128;
+    double v = (i[b] - y) * 0.564 + 128;
     PixelValues p;
-    p.y = rgbPixel[0];
-    p.u = rgbPixel[1];
-    p.v = rgbPixel[2];
+    p.y = static_cast<uint8_t>(y); 
+    p.u = static_cast<uint8_t>(u);
+    p.v = static_cast<uint8_t>(v);
     return p;
 }
 
@@ -27,7 +33,7 @@ int main(int argc, char** argv) {
     std::vector<int> featureColours; // list of colours for each feature
     std::vector<cv::Rect> features; // list of bounding boxes
     std::vector<cv::Mat> featuresExtracted; // list of extracted foreground images, bg is black 
-#if 0
+#if 1
     // 005.png T fieldline + Ball
 
     // Ball 005.png
