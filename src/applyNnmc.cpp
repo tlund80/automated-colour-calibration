a4c9900902c76af035e3ef43183734dc7148b4ea
Calvin Tam <calvinyhtam@gmail.com>
2013-05-20 11:16:23 +1000

    Added applyNnmc.cpp and compareError.cpp

---
+++ src/applyNnmc.cpp
@@ -0,0 +1,18 @@
+/*
+ * applyNnmc.cpp
+ * Given an .nnmc file, apply it to the given image
+ * By Calvin Tam
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        cerr << "Usage: ./applyNnmc IMAGE_PATH NNMC_PATH" << endl;
+        exit(1);
+    }
+    // pass nnmc to runswift code
+
+}
