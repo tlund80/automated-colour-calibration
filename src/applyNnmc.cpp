8a0247b6eebd4f03b2f1aab78b1165643b125131
Calvin Tam <calvinyhtam@gmail.com>
2013-05-20 15:05:35 +1000

    Added NNMC runswift code

--- src/applyNnmc.cpp
+++ src/applyNnmc.cpp
@@ -4,15 +4,46 @@
  * By Calvin Tam
  *
  */
-
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include "NNMC.hpp"
+#include "NNMCinline.hpp"
+using namespace cv;
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if(argc != 3) {
-        cerr << "Usage: ./applyNnmc IMAGE_PATH NNMC_PATH" << endl;
+    if(argc != 4) {
+        cerr << "Usage: ./applyNnmc IMAGE_PATH OUTPUT_IMAGE_PATH NNMC_PATH" << endl;
         return 1;
     }
-    // pass nnmc to runswift code
-
+    
+    cv::Mat origImage = imread(argv[1],1);
+    cv::Mat saliencyImage = origImage.clone();
+    // Pass nnmc to runswift code
+    NNMC nnmc;
+    nnmc.load(argv[3]);
+    if(!(nnmc.isLoaded())) {
+        cerr << "NNMC file not loaded correctly" << endl;
+        return 1; 
+    }
+    else {
+        for(int y = 0; y < origImage.rows; ++y) {
+            for(int x = 0; x < origImage.cols; ++x) {
+                /*
+                PixelValues p = rgb2yuv(origImage.at<Vec3b>(y,x));
+                Colour classified = nnmc.classify(p.y,p.u,p.v);
+                Vec3b rgb = convertColourToVec3b(classified);
+                saliencyImage.at<Vec3b>(y,x) = rgb;
+                */
+            }
+        }
+    }
+    // Save the colour classified saliency image
+    imwrite(argv[2],saliencyImage); 
+    cv::namedWindow(argv[1],CV_WINDOW_NORMAL);
+    cv::imshow(argv[1],origImage);
+    cv::namedWindow(argv[2], CV_WINDOW_NORMAL);
+    cv::imshow(argv[2],saliencyImage);
+    return 0;
 }
