867e007f5deece7d3a456c00cef490413063f416
Calvin Tam <calvinyhtam@gmail.com>
2013-05-22 22:17:43 +1000

    Working version of runExperiment.sh

--- src/runExperiment.sh
+++ src/runExperiment.sh
@@ -3,6 +3,7 @@ BUILD_DIR="../build"
 
 FILL_UNCLASSIFIED_EXEC="$BUILD_DIR/fillUnclassified"
 APPLY_NNMC_EXEC="$BUILD_DIR/applyNnmc"
+GRABCUT_EXEC="$BUILD_DIR/grabCut"
 COMPARE_ERROR_EXEC="$BUILD_DIR/compareError"
 
 
@@ -16,6 +17,15 @@ function myecho {
 function confirm {
     echo -n "Done with the previous? [Enter to continue, Ctrl-C to exit] "
     read done
+}
+
+function askExecute {
+    echo -n "Execute? ['y' to execute, Enter to skip] "
+    read execute
+    if [ "$execute" == "y" ]
+    then
+        eval "$1"
+    fi
 }
 
 set -e
@@ -33,53 +43,65 @@ fi
 INPUT_DIR=`dirname $IMAGE_PATH`
 origImage="$IMAGE_PATH"
 smallOrigImage="$INPUT_DIR/small_original.png"
-echo "Downsampling from 640x480 to 160x120 ($origImage ---> $smallOrigImage)"
-convert $origImage -sample 160x120 $smallOrigImage
+echo "Downsample from 640x480 to 160x120 ($origImage ---> $smallOrigImage)"
+askExecute 'convert $origImage -sample 160x120 $smallOrigImage'
 nnmc_manual="$INPUT_DIR/manual.nnmc"
-echo "Calibrate and save nnmc file as " && myecho $nnmc_manual
-confirm
+echo -n "Calibrate and save nnmc file as " && myecho $nnmc_manual
+askExecute
 classifiedManualImage="$INPUT_DIR/manual.png"
-echo "Applying the nnmc file ($smallOrigImage ---> $classifiedManualImage)"
-eval $APPLY_NNMC_EXEC $smallOrigImage $classifiedManualImage $nnmc_manual
+echo "Apply the nnmc file ($smallOrigImage ---> $classifiedManualImage)"
+askExecute '$APPLY_NNMC_EXEC $smallOrigImage $classifiedManualImage $nnmc_manual'
 echo "Manual Calibration Complete"
 
+askExecute
 echo
-confirm
 
 myecho "==== Ground Truth ===="
 classifiedTruthImage="$INPUT_DIR/partial_truth.png"
-echo "Copying ($smallOrigImage ---> $classifiedTruthImage)"
-cp $smallOrigImage $classifiedTruthImage
+echo "Copy ($smallOrigImage ---> $classifiedTruthImage)"
+askExecute 'cp $smallOrigImage $classifiedTruthImage'
 echo -n "Go to GIMP and classify " && myecho $classifiedTruthImage 
-confirm
+askExecute
 fillClassifiedTruthImage="$INPUT_DIR/truth.png"
-echo "Filling in unclassified pixels ($classifiedTruthImage ---> $fillClassifiedTruthImage)"
-eval $FILL_UNCLASSIFIED_EXEC $classifiedTruthImage $fillClassifiedTruthImage
+echo "Fill in unclassified pixels ($classifiedTruthImage ---> $fillClassifiedTruthImage)"
+askExecute '$FILL_UNCLASSIFIED_EXEC $classifiedTruthImage $fillClassifiedTruthImage'
 echo "Ground Truth Complete"
 
+askExecute
 echo
-confirm
 
 myecho "==== GrabCut ===="
 echo "Open $origImage in GIMP"
 grabCutTemplate="$INPUT_DIR/features.template"
-echo "Write GrabCut template as " && myecho $grabCutTemplate
-confirm
-nnmc_grabcut="$INPUT_DIR/grabcut.nnmc"
-echo "Applying Grabcut"
-eval $GRABCUT_EXEC $origImage $grabCutTemplate $nnmc_grabcut
+echo -n "Write GrabCut template as " && myecho $grabCutTemplate
+askExecute
+echo "Apply GrabCut template"
 classifiedGrabcutImage="$INPUT_DIR/grabcut.png"
-echo "Applying the nnmc file ($smallOrigImage ---> $classifiedGrabcutImage)"
-eval $APPLY_NNMC_EXEC $smallOrigImage $classifiedGrabcutImage $nnmc_grabcut
+askExecute '
+    happy="no"
+    until [ "$happy" == "y" ]
+    do
+        echo -n "Update the GrabCut template " && myecho $grabCutTemplate
+        askExecute
+        nnmc_grabcut="$INPUT_DIR/grabcut.nnmc"
+        echo "Apply Grabcut"
+        askExecute "$GRABCUT_EXEC $origImage $grabCutTemplate $nnmc_grabcut"
+        echo "Apply the nnmc file ($smallOrigImage ---> $classifiedGrabcutImage)"
+        askExecute "$APPLY_NNMC_EXEC $smallOrigImage $classifiedGrabcutImage $nnmc_grabcut"
+        echo "Happy with the grabcut results? [\"y\" to finish, else redo template] "
+        read happy
+    done
+'
 echo "Grabcut Complete"
 
+askExecute
 echo
-confirm
 
 myecho "==== Compare Errors ===="
+echo "== Error Rates in % =="
 echo -e "Manual\tAutomatic"
 echo -e "======\t========="
 echo -n `eval $COMPARE_ERROR_EXEC $fillClassifiedTruthImage $classifiedManualImage`
-echo -n "\t"
+echo -en "\t"
 echo -n `eval $COMPARE_ERROR_EXEC $fillClassifiedTruthImage $classifiedGrabcutImage`
-
+echo
